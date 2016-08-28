c41825ae0ee78c7d429fb2e68d378220dbe07fde
LK <lenny.khazan@gmail.com>
2016-08-28 19:33:44 -0400

    Initial Commit

---
+++ MathParser/MathParser.h
@@ -0,0 +1,19 @@
+//
+//  MathParser.h
+//  MathParser
+//
+//  Created by Lenny Khazan on 8/28/16.
+//  Copyright © 2016 Lenny Khazan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MathParser.
+FOUNDATION_EXPORT double MathParserVersionNumber;
+
+//! Project version string for MathParser.
+FOUNDATION_EXPORT const unsigned char MathParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathParser/PublicHeader.h>
+
+
